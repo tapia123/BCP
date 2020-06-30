aff05c720e08a858eba521debb0896b8b079a058
tapia123 <67613452+tapia123@users.noreply.github.com>
2020-06-29 20:10:07 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:firebase_admob/firebase_admob.dart';
 import 'package:admob_flutter/admob_flutter.dart';
 
 void main (){
-  WidgetsFlutterBinding.ensureInitialized();
-  FirebaseAdMob.instance.initialize(appId: 'ca-app-pub-7270824244598271~8029392745');
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarIconBrightness: Brightness.dark,
@@ -15,17 +13,6 @@ void main (){
       statusBarBrightness: Brightness.dark)
   );
   runApp(Myapp());
-  myBanner
-  // typically this happens well before the ad is shown
-  ..load()
-  ..show(
-    // Positions the banner ad 60 pixels from the bottom of the screen
-    anchorOffset: 0.0,
-    // Positions the banner ad 10 pixels from the center of the screen to the right
-    horizontalCenterOffset: 0.0,
-    // Banner Position
-    anchorType: AnchorType.bottom,
-  );
 }
 
 class Myapp extends StatelessWidget {
@@ -40,18 +27,3 @@ class Myapp extends StatelessWidget {
     );
   }
 }
-
-MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-  keywords: <String>['BANCO BCP', 'beneficios BCP'],
-  childDirected: false,
-  testDevices: <String>[], // Android emulators are considered test devices
-);
-
-BannerAd myBanner = BannerAd(
-  adUnitId: 'ca-app-pub-7270824244598271/3514792034',
-  size: AdSize.smartBanner,
-  targetingInfo: targetingInfo,
-  listener: (MobileAdEvent event) {
-    print("BannerAd event is $event");
-  },
-);
